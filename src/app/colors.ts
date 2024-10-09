55d9aadb19e3da9d8dc1c0994756ef057b1f5767
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-09 20:36:21 -0300

    feat: add a 600 primary color

--- src/app/colors.ts
+++ src/app/colors.ts
@@ -1,9 +1,20 @@
+
 export const colors = {
   background: "hsl(0 0% 100%)",
   foregroud: "hsl(224 71.4% 4.1%)",
   border: "hsl(220 13% 91%)",
   primary: {
+    "600": "#6440e7",
     "700": "#542ed3",
     "950": "#231362",
-  }
-}
+  },
+
+};
+export const theme = {
+  colors: {
+    primary: colors.primary[700],
+    background: colors.background,
+    text: colors.foregroud,
+    accent: colors.primary[600],
+  },
+};
