d92c686eecab66ec5a463fbcd87b989870c065b2
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-09 20:36:33 -0300

    feat: add a theme provider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/app/home';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/app/home";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "@/app/colors";
 
 export default function App() {
-  return (
-    <Home />
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Home />
+		</ThemeProvider>
+	);
 }
-
