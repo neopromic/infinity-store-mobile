0432ec15a65ba2c3b1c67adc6260efdd74d5095e
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-09 21:29:16 -0300

    feat: add a backgroundInverted color

--- src/app/colors.ts
+++ src/app/colors.ts
@@ -1,6 +1,6 @@
-
 export const colors = {
   background: "hsl(0 0% 100%)",
+  backgroundInvert: "hsl(224 71.4% 4.1%)",
   foregroud: "hsl(224 71.4% 4.1%)",
   border: "hsl(220 13% 91%)",
   primary: {
@@ -8,12 +8,12 @@ export const colors = {
     "700": "#542ed3",
     "950": "#231362",
   },
-
 };
 export const theme = {
   colors: {
     primary: colors.primary[700],
     background: colors.background,
+    backgroundInvert: colors.backgroundInvert,
     text: colors.foregroud,
     accent: colors.primary[600],
   },
