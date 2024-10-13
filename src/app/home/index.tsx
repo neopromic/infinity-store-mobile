1fe2edee8a48ee88f611abede980762316dc268c
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-13 00:05:29 -0300

    feat: using a new product card component

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,4 +1,4 @@
-import { StatusBar, View } from "react-native";
+import { FlatList, ScrollView, StatusBar, View } from "react-native";
 import {
 	Button,
 	ButtonText,
@@ -10,30 +10,67 @@ import {
 	SearchProductsView,
 } from "./styles";
 import { colors } from "@/app/colors";
-import { Flame, ShoppingCart } from "phosphor-react-native";
 import { material } from "react-native-typography";
+import { default as IconAntDesign } from "react-native-vector-icons/AntDesign";
+import { default as IconIonicons } from "react-native-vector-icons/Ionicons";
+import { ProductCard, type Product } from "@/components/ProductCard";
+
+const products: Product[] = [
+	{
+		id: 0,
+		name: "Produto 1",
+		price: "29.99",
+		image: "https://github.com/neopromic.png",
+	},
+	{
+		id: 1,
+		name: "Produto 2",
+		price: "49.99",
+		image: "https://github.com/neopromic.png",
+	},
+	{
+		id: 2,
+		name: "Produto 3",
+		price: "19.99",
+		image: "https://github.com/neopromic.png",
+	},
+	{
+		id: 3,
+		name: "Produto 4",
+		price: "59.99",
+		image: "https://github.com/neopromic.png",
+	},
+	{
+		id: 4,
+		name: "Produto 5",
+		price: "59.99",
+		image: "https://github.com/neopromic.png",
+	},
+	{
+		id: 5,
+		name: "Produto 6",
+		price: "59.99",
+		image: "https://github.com/neopromic.png",
+	},
+];
 
 export function Home() {
 	return (
 		<Container>
-			{/* Status bar configurada para combinar com o background */}
 			<StatusBar backgroundColor={colors.background} />
 			<HeaderView>
-				{/* Campo de busca */}
 				<SearchProductsView
 					autoFocus={false}
 					placeholder="Procure por produtos..."
 				/>
 
-				{/* Botão do carrinho de compras */}
 				<Button>
 					<ButtonText>
-						<ShoppingCart />
+						<IconAntDesign name="shoppingcart" size={24} />
 					</ButtonText>
 				</Button>
 			</HeaderView>
-
-			{/* Seção de produtos */}
+<ScrollView>
 			<ProductSectionView>
 				<View
 					style={{
@@ -43,18 +80,28 @@ export function Home() {
 						width: "100%",
 						padding: 8,
 						alignContent: "center",
+						gap: 8,
 					}}
 				>
-					<Flame
-						size={32}
-						style={{ marginRight: 8 }}
-						color={colors.primary[700]}
-					/>
+					<IconIonicons name="flame" size={24} color={colors.primary[700]} />
 					<ProductSectionTitle style={material.headline}>
 						Lojas recomendadas
 					</ProductSectionTitle>
 				</View>
+
+				<FlatList
+					data={products}
+					keyExtractor={(item) => item.id.toString()}
+					numColumns={2}
+					renderItem={({ item }) => (
+							<View style={{ flex: 1, margin: 8 }}>
+								<ProductCard product={item} />
+							</View>
+					)}
+					contentContainerStyle={{ paddingHorizontal: 8 }}
+				/>
 			</ProductSectionView>
+			</ScrollView>
 		</Container>
 	);
 }
