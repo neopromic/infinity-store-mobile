e7eb553488897e5c3c2e3d4616d20c7e3e6ab290
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-13 12:28:54 -0300

    feat: add types globally

---
+++ src/types/products.ts
@@ -0,0 +1,12 @@
+export interface Product {
+	id: number;
+	name: string;
+	price: string;
+	image: string;
+}
+
+export interface Business  {
+	id: number | string
+	businessName: string
+	businessLogo: string
+}
