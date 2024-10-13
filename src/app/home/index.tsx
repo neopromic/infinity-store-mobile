e5814f6f86df76bdec9ab442b9c51dd8da65febb
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-13 20:41:09 -0300

    feat: removed unnecessary things

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,56 +1,65 @@
-import { FlatList, ScrollView, StatusBar, View } from "react-native";
+import { FlatList, ScrollView, StatusBar, View, Text } from "react-native";
 import {
 	Button,
 	ButtonText,
 	Container,
 	DefaultText,
 	HeaderView,
-	ProductSectionTitle,
-	ProductSectionView,
 	SearchProductsView,
+	SectionTitle,
+	SectionView,
 } from "./styles";
 import { colors } from "@/app/colors";
 import { material } from "react-native-typography";
 import { default as IconAntDesign } from "react-native-vector-icons/AntDesign";
 import { default as IconIonicons } from "react-native-vector-icons/Ionicons";
-import { ProductCard, type Product } from "@/components/ProductCard";
+import { ProductCard } from "@/components/ProductCard";
+import { ShopCard } from "@/components/ShopsCard";
+import type { Business, Product } from "@/types/products";
 
 const products: Product[] = [
 	{
 		id: 0,
-		name: "Produto 1",
-		price: "29.99",
 		image: "https://github.com/neopromic.png",
+		name: "Ferrari",
+		price: "200",
 	},
 	{
 		id: 1,
-		name: "Produto 2",
-		price: "49.99",
 		image: "https://github.com/neopromic.png",
+		name: "Ferrari",
+		price: "200",
 	},
 	{
 		id: 2,
-		name: "Produto 3",
-		price: "19.99",
 		image: "https://github.com/neopromic.png",
+		name: "Ferrari",
+		price: "200",
 	},
 	{
 		id: 3,
-		name: "Produto 4",
-		price: "59.99",
 		image: "https://github.com/neopromic.png",
+		name: "Ferrari",
+		price: "200",
 	},
 	{
 		id: 4,
-		name: "Produto 5",
-		price: "59.99",
 		image: "https://github.com/neopromic.png",
+		name: "Ferrari",
+		price: "200",
+	},
+];
+
+const recommendedStores: Business[] = [
+	{
+		id: 0,
+		businessName: "BikeShop",
+		businessLogo: "https://github.com/neopromic.png"
 	},
 	{
-		id: 5,
-		name: "Produto 6",
-		price: "59.99",
-		image: "https://github.com/neopromic.png",
+		id: 1,
+		businessName: "Barber for Mens",
+		businessLogo: "https://github.com/neopromic.png"
 	},
 ];
 
@@ -70,37 +79,73 @@ export function Home() {
 					</ButtonText>
 				</Button>
 			</HeaderView>
-<ScrollView>
-			<ProductSectionView>
-				<View
-					style={{
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-						width: "100%",
-						padding: 8,
-						alignContent: "center",
-						gap: 8,
-					}}
-				>
-					<IconIonicons name="flame" size={24} color={colors.primary[700]} />
-					<ProductSectionTitle style={material.headline}>
-						Lojas recomendadas
-					</ProductSectionTitle>
-				</View>
 
-				<FlatList
-					data={products}
-					keyExtractor={(item) => item.id.toString()}
-					numColumns={2}
-					renderItem={({ item }) => (
+			<ScrollView>
+				{/* Seção Mais Vendidos */}
+				<SectionView>
+					<View
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							width: "100%",
+							padding: 8,
+							gap: 8,
+						}}
+					>
+						<IconIonicons name="flame" size={24} color={colors.primary[700]} />
+						<SectionTitle style={material.headline}>Mais vendidos</SectionTitle>
+					</View>
+
+					<FlatList
+						data={products}
+						keyExtractor={(item) => item.id.toString()}
+						numColumns={2}
+						renderItem={({ item }) => (
 							<View style={{ flex: 1, margin: 8 }}>
 								<ProductCard product={item} />
 							</View>
-					)}
-					contentContainerStyle={{ paddingHorizontal: 8 }}
-				/>
-			</ProductSectionView>
+						)}
+						scrollEnabled={false} // Para garantir que o FlatList não tente rolar
+						contentContainerStyle={{ paddingHorizontal: 8 }}
+					/>
+				</SectionView>
+
+				{/* Seção Lojas Recomendadas */}
+				<SectionView>
+					<View
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							width: "100%",
+							padding: 8,
+							gap: 8,
+						}}
+					>
+						<IconIonicons
+							name="storefront"
+							size={24}
+							color={colors.primary[700]}
+						/>
+						<SectionTitle style={material.headline}>
+							Lojas recomendadas
+						</SectionTitle>
+					</View>
+
+					<FlatList
+						data={recommendedStores}
+						keyExtractor={(item) => item.id.toString()}
+						renderItem={({ item }) => (
+							<View style={{ flex: 1, margin: 8, width: 200 }}>
+								<ShopCard businessData={item} />
+							</View>
+						)}
+						horizontal={true}
+						showsHorizontalScrollIndicator
+						contentContainerStyle={{ paddingHorizontal: 8, paddingBottom: 100 }}
+					/>
+				</SectionView>
 			</ScrollView>
 		</Container>
 	);
